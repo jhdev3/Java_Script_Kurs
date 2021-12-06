672cfebfc672418f5be28718fd7b6b023aced402
Jacob Hedén <91886824+jhdev3@users.noreply.github.com>
2021-12-06 14:02:38 +0100

    Chat
    Starten på chat

---
+++ Chat/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/chat.html");
+});
+
+io.emit("some event", {
+  someProperty: "some value",
+  otherProperty: "other value",
+}); // This will emit the event to all connected sockets
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
